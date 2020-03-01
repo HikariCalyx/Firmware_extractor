e5f822020e4b372b22f79600fc2ff8be56c534bd
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2020-03-01 12:24:15 +0800

    Update mover.sh

--- mover.sh
+++ mover.sh
@@ -14,31 +14,31 @@ mv *-boot.img ../${fwver}_process/boot.img
 mv *-dtbo.img ../${fwver}_process/dtbo.img
 mv *-vbmeta.img ../${fwver}_process/vbmeta.img
 if [ "$mtkprldr" ]; then
-echo WARNING: MediaTek model detected. The preloader partition may not being processed correctly. I've tried my best to make it happen.
-prldrsize=`ls -l $mtkprldr | awk '{print $5}'`
-cat ../prldrhdr_256 ${mtkprldr} >prldr1.img
-if [ $projectcode -eq PDA ]; then
-    dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=262240
-    dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
-    else if [ $projectcode -eq ROO ]; then
-        dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=258048
+    echo WARNING: MediaTek model detected. The preloader partition may not being processed correctly. I've tried my best to make it happen.
+    prldrsize=`ls -l $mtkprldr | awk '{print $5}'`
+    cat ../prldrhdr_256 ${mtkprldr} >prldr1.img
+    if [ $projectcode -eq PDA ]; then
+        dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=262240
         dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
-        else if [ $projectcode -eq ES2 ]; then
+        else if [ $projectcode -eq ROO ]; then
             dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=258048
             dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
-            else if [ $projectcode -eq CO2 ]; then
+            else if [ $projectcode -eq ES2 ]; then
                 dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=258048
                 dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
-            else
-                dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=262144
-                dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
+                else if [ $projectcode -eq CO2 ]; then
+                    dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=258048
+                    dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
+                else
+                    dd if=dev/zero of=../${fwver}_process/preloader.img bs=1 count=262144
+                    dd if=prldr.img of=../${fwver}_process/preloader.img conv=notrunc
+                fi
             fi
         fi
     fi
+    rm prldr1.img
+    rm ${mtkprldr}
 fi
-fi
-rm prldr1.img
-rm ${mtkprldr}
 grep -a RADIO systeminfo.img|awk -F, '{print $7,$11}'>move.sh
 sed -i 's|tar.gz|img|g' move.sh
 sed -i "s|SYSTEM/product:||g" move.sh
